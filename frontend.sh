4b10f348fbe830d8aa83030f2d7763ebb0179ebe
koushik <koushik214@gmail.com>
2023-09-13 22:54:11 +0530

    updated echo command and added redirected log

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,16 @@
 echo Installing Nginx
 
-dnf install nginx -y > /tmp/expense.log
+dnf install nginx -y >> /tmp/expense.log
 
-cp frontend.conf /etc/nginx/default.d/expense.conf > /tmp/expense.log
+cp frontend.conf /etc/nginx/default.d/expense.conf >> /tmp/expense.log
 
-rm -rf /usr/share/nginx/html/* > /tmp/expense.log
+rm -rf /usr/share/nginx/html/* >> /tmp/expense.log
 
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip > /tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/expense.log
 
-cd /usr/share/nginx/html > /tmp/expense.log
+cd /usr/share/nginx/html >> /tmp/expense.log
 
-unzip /tmp/frontend.zip > /tmp/expense.log
-systemctl enable nginx > /tmp/expense.log
+unzip /tmp/frontend.zip >> /tmp/expense.log
+systemctl enable nginx >> /tmp/expense.log
 
-systemctl restart nginx > /tmp/expense.log
+systemctl restart nginx >> /tmp/expense.log
