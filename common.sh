7c05c65792de09e9c20d9f3c1be79eadc7e3bdcc
koushik <koushik214@gmail.com>
2023-09-14 22:39:02 +0530

    expense shell

--- common.sh
+++ common.sh
@@ -5,11 +5,11 @@ download_and_extract()
 
 echo  Download $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
-echo $?
+stat_check
 
 echo extract $component code
 unzip /tmp/$component.zip >> $log_file
-echo $?
+stat_check
 }
 
 stat_check()
