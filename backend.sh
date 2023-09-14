465b87f47ea2c891c867f19c5bf597c336b7b37c
koushik <koushik214@gmail.com>
2023-09-14 19:14:33 +0530

    updated exit status for backend.sh and common.sh

--- backend.sh
+++ backend.sh
@@ -5,11 +5,15 @@ component=backend
 echo executing backend scripts
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
 
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 dnf install nodejs -y &>>$log_file
+echo $?
 
 useradd expense &>>$log_file
+echo $?
 
 mkdir /app &>>$log_file
 cd /app
@@ -18,10 +22,16 @@ download_and_extract
 
 
 npm install &>>$log_file
+echo $?
 systemctl daemon-reload &>>$log_file
+echo $?
 systemctl enable backend &>>$log_file
+echo $?
 systemctl start backend &>>$log_file
+echo $?
 
 dnf install mysql -y &>>$log_file
+echo $?
 
 mysql -h mysql.kdevopsb75.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
--- common.sh
+++ common.sh
@@ -5,7 +5,9 @@ download_and_extract()
 
 echo  Download $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
+echo $?
 
 echo extract $component code
 unzip /tmp/$component.zip >> $log_file
+echo $?
 }
